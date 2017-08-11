b0de9010d20fdc2db1a28a8df78d91a3e93542ff
TaQi <TQ@192.168.3.14>
2017-08-11 10:59:51 +0800

    lxc

---
+++ UtilFramework/Helpers/Json.swift
@@ -0,0 +1,17 @@
+//
+//  Json.swift
+//  UtilFramework
+//
+//  Created by TaQi on 2017/8/11.
+//  Copyright © 2017年 TQMac. All rights reserved.
+//
+
+import Foundation
+
+public class Json :NSObject{
+    
+    public func ToJson(obj:NSObject) ->String{
+        return "LXHSD";
+    }
+    
+}
