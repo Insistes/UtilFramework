2e57db0cea426d90a85b274a2ecee7f81f830d6e
lixinhua <982839499@qq.com>
2017-08-11 10:51:18 +0800

    21

---
+++ UtilFramework/UtilFramework.h
@@ -0,0 +1,19 @@
+//
+//  UtilFramework.h
+//  UtilFramework
+//
+//  Created by TQMac on 2017/8/11.
+//  Copyright © 2017年 TQMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtilFramework.
+FOUNDATION_EXPORT double UtilFrameworkVersionNumber;
+
+//! Project version string for UtilFramework.
+FOUNDATION_EXPORT const unsigned char UtilFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilFramework/PublicHeader.h>
+
+///////lxh
